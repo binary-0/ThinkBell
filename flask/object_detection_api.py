96b411412277e1f61bca62e838e7f7d644586c83
Jinyoung Lee <50437138+binary-0@users.noreply.github.com>
2021-11-08 00:42:09 +0900

    Final: Integration Complete - with CV module

--- flask/object_detection_api.py
+++ flask/object_detection_api.py
@@ -12,12 +12,133 @@ import json
 from keras.models import load_model
 from MediaPipeProcess import mediapipe_process
 import daiseecnn
+import time
+from queue import Queue
 
 
 # if tf.__version__ != '1.4.0':
 #   raise ImportError('Please upgrade your tensorflow installation to v1.4.0!')
 
 # ENV SETUP  ### CWH: remove matplot display and manually add paths to references
+
+class detectionQueue:
+    def __init__(self):
+        self.que = Queue()
+        self.size = 0
+        self.sum = 0
+        self.avg = 0
+
+        self.colorCnt = [0, 0, 0] #red, creem, green
+
+        self.maxSize = 5
+
+    def detectionPush(self, data):
+        if data is 0 or data is 1:
+            self.colorCnt[0] += 1
+        elif data is 2:
+            self.colorCnt[1] += 1
+        else:
+            self.colorCnt[2] += 1
+
+        if self.size < self.maxSize:
+            self.que.put(data)
+
+            self.size += 1
+
+            return self.colorCnt.index(max(self.colorCnt))
+        else:
+            deq = self.que.get()
+            if deq is 0 or deq is 1:
+                self.colorCnt[0] -= 1
+            elif deq is 2:
+                self.colorCnt[1] -= 1
+            else:
+                self.colorCnt[2] -= 1
+
+            self.que.put(data)
+
+            return self.colorCnt.index(max(self.colorCnt))
+
+##############
+##############
+global daisee
+global prevGestureIdx
+global frCnt
+global CALITIME
+global caliEnd
+global startTime
+global prevTime
+global TIMETHRESHOLD
+global CNNTime
+global headSum
+global headAvg
+global headPoseSum
+global headPoseAvg
+global EARSum
+global EARAvg
+global EARTime
+global dQ
+global firstImg
+global predOnce
+global faceAddedTime
+global EARAddedTime
+global curTime
+global sec
+
+global headStatus
+global headPoseStatus
+global handGestureStatus
+global EARStatus
+
+daisee = daiseecnn.DaiseeCNN()
+prevGestureIdx = -1
+
+frCnt = 0
+
+CALITIME = 13
+caliEnd = False
+
+startTime = time.time()
+prevTime = startTime
+TIMETHRESHOLD = 3
+
+CNNTime = None
+
+headSum = 0
+headAvg = None
+
+headPoseSum = [0, 0]
+headPoseAvg = [None, None]
+
+EARSum = 0
+EARAvg = None
+EARTime = None
+
+dQ = detectionQueue()
+
+firstImg = cv2.imread('./FirstImage.jpg')
+predOnce = False
+
+faceAddedTime = 0
+EARAddedTime = 0
+
+curTime = time.time()
+sec = curTime - prevTime
+prevTime = curTime
+
+headStatus = None
+headPoseStatus = None
+EARStatus = False
+handGestureStatus = -1
+
+ERROR_OUTPUT = json.dumps({
+    'colorStat': '-1',
+    'generalStat': '-1',
+    'handStat': '-1'
+})
+
+############
+############
 
 # added to put object in JSON
 class Object(object):
@@ -33,41 +154,188 @@ def load_image_into_numpy_array(image):
       (im_height, im_width, 3)).astype(np.uint8)
 
 def get_objects(image, threshold=0.5):
-    # print(type(image))
+    global daisee
+    global prevGestureIdx
+    global frCnt
+    global CALITIME
+    global caliEnd
+    global startTime
+    global prevTime
+    global TIMETHRESHOLD
+    global CNNTime
+    global headSum
+    global headAvg
+    global headPoseSum
+    global headPoseAvg
+    global EARSum
+    global EARAvg
+    global EARTime
+    global dQ
+    global firstImg
+    global predOnce
+    global faceAddedTime
+    global EARAddedTime
+    global curTime
+    global sec
+    
+    global headStatus
+    global headPoseStatus
+    global handGestureStatus
+    global EARStatus
 
+    curTime = time.time()
+    sec = curTime - prevTime
+    prevTime = curTime
 
-    daisee = daiseecnn.DaiseeCNN()
+    if predOnce is False:
+        print("predictMae")
+        dummy = daisee.prediction(firstImg)
+        predOnce = True
+        startTime = time.time()
+        return ERROR_OUTPUT #Send 오류코드
 
     image_np = load_image_into_numpy_array(image)
-    rgbFrame = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-
-    daisee_prediction = daisee.prediction(image_np)
-
-    print("예측 : ", daisee_prediction)
-
-    EAR, headArea, handLM, poseLM = mediapipe_process(rgbFrame, rgbFrame, rgbFrame)
-
-    print(f'EAR:{EAR} / headArea:{headArea}')
-
-    # ouput={}
-    # ouput['predict']=daisee_prediction
-    # ouput['EAR']=EAR
-    # ouput['headArea']=headArea
-    # print(type(daisee_prediction), type(EAR), type(headArea))
-    # outputJson = json.dumps({[1,2,3]})
-
+    frame = image_np
+    if frame is None:
+        return ERROR_OUTPUT #Send 오류코드
     
-    if not isinstance(daisee_prediction, int) :
-        Dout=daisee_prediction.item()
-    else:
-        Dout=daisee_prediction
+    frCnt += 1
+    try:
+        print('FPS: ' + str(1 / (sec)))
+    except:
+        print('FPS___')
 
-    if not isinstance(EAR, int):
-        Eout=EAR.item()
-    else:
-        Eout=EAR
+    try:
+        rgbFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    except:
+        print("Couldn't grapped. Maybe video is changing..")
+        return ERROR_OUTPUT #Send 오류코드
 
-    outputJson = json.dumps({"predict":Dout, "EAR":Eout, "headarea":headArea})
+    ### FOR YOLO ###
+    
+    handGestureTemp = -1
+    
+    EAR, headArea, headPose, handLM, poseLM, handGesIdx = mediapipe_process(rgbFrame, rgbFrame, rgbFrame)
+
+    handGestureTemp = handGesIdx
+    #print(f'prev:{prevGestureIdx[i]} and cur:{handGestureTemp}')
+    if handGestureTemp is not -1 and prevGestureIdx == handGestureTemp:
+        #print(f'//////////haittaa with {handGestureTemp}')
+        if handGestureTemp == 5 or handGestureTemp == 9 or handGestureTemp == 10:
+            handGestureStatus = handGestureTemp
+        prevGestureIdx = handGestureTemp
+    else:
+        handGestureStatus = -1
+        prevGestureIdx = handGestureTemp
+
+    if caliEnd is False:
+        if time.time() - startTime > CALITIME: # CALIBRATION DONE
+            headAvg = headSum / faceAddedTime
+            EARAvg = EARSum / EARAddedTime
+            headPoseAvg[0] = headPoseSum[0] / faceAddedTime
+            headPoseAvg[1] = headPoseSum[1] / faceAddedTime
+            
+            caliEnd = True
+            print('////////////////////CALIEND//////////')
+        else:
+            if EAR is not -1 and headArea is not -1:
+                headSum += headArea
+                headPoseSum[0] += headPose[0]
+                headPoseSum[1] += headPose[1]
+                faceAddedTime += 1
+                EARSum += EAR
+                EARAddedTime += 1
+    else: #caliEnd is True:
+        if EAR is not -1 and headArea is not -1:
+            if headArea < headAvg * 0.75:
+                headStatus = 2
+            else:
+                headStatus = 3
+
+            #0: forward
+            #1: horizontal movement
+            #2: vertical(upper) movement
+            if headPose[0] < headPoseAvg[0]-7 or headPose[0] > headPoseAvg[0]+7:
+                headPoseStatus = 1
+            elif headPose[1] > headPoseAvg[1]+7:
+                headPoseStatus = 2
+            else:
+                headPoseStatus = 0
+
+            earCurTime = time.time()
+            if EAR < EARAvg * 0.875:
+                if EARTime is None:
+                    EARTime = earCurTime
+                else:
+                    if earCurTime - EARTime > 3:#TIMETHRESHOLD
+                        EARStatus = True
+            else:
+                #EARNOTDETECTEDTHISTIME
+                EARStatus = False
+                EARTime = None
+        else:
+            headStatus = 1
+            EARStatus = False #Controversial!!!
+    ### FOR YOLO ###
+    
+    local_prediction = int(daisee.prediction(frame))
+        
+    if headStatus is 1 or headStatus is 2 or headPoseStatus is 1 or headPoseStatus is 2:
+        local_prediction = 0
+    elif EARStatus is True:
+        local_prediction = 0
+
+    predEngage = dQ.detectionPush(local_prediction)
+    if predEngage is 0 or predEngage is 1: #Not engaged
+        colorStatus = 0
+    elif predEngage is 2: #Not engaged
+        colorStatus = 1
+    else: #Not engaged
+        colorStatus = 3
+
+    generalStatus = [False, False, False, False]
+    if headStatus is None:
+        pass
+    elif headStatus is 1:
+        generalStatus[0] = True
+        generalStatus[1] = False
+    elif headStatus is 2:
+        generalStatus[0] = False
+        generalStatus[1] = True    
+    else:
+        generalStatus[0] = False
+        generalStatus[1] = False
+
+    if headStatus is not 1 and generalStatus[1] is False: #When face is detected
+        if headPoseStatus is None:
+            pass
+        elif headPoseStatus is 1:
+            generalStatus[1] = True
+        elif headPoseStatus is 2:
+            generalStatus[1] = True
+        else:
+            generalStatus[1] = False
+
+    if EARStatus is True:
+        generalStatus[3] = True
+    else:
+        generalStatus[3] = False
+
+    returnVal = [0, 0, 0, 0]
+    if generalStatus[0] is True:
+        returnVal[0] = 1
+    if generalStatus[1] is True:
+        returnVal[1] = 1
+    if generalStatus[2] is True:
+        returnVal[2] = 1
+    if generalStatus[3] is True:
+        returnVal[3] = 1
+
+    outputJson = json.dumps({
+        'colorStat': str(colorStatus),
+        'generalStat': f'{returnVal[0]}{returnVal[1]}{returnVal[2]}{returnVal[3]}',
+        'handStat': f'{handGestureStatus}'
+        })
+    print(outputJson)
     return outputJson
+
