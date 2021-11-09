41e28c2a64d8123221a5d7006147437e6086fddd
Siyeol Choi <siyeol13@naver.com>
2021-11-10 01:40:03 +0900

    Automated Broadcasting
    broadcast every 5 seconds with name, also save content into array

--- webrtc/public/js/broadcast.js
+++ webrtc/public/js/broadcast.js
@@ -1,19 +1,31 @@
+let engLog = [];
 
 function getStatus() {
 
     console.log("get try");
+    setInterval(sendStatus, 5000);
+
     // // var output = {sender:"sender", receiver:"receiver"
     // //     ,command:"chat", type:"text", data:"msg"};
     // socket.emit("message", content);
     socket.on('chat', message=>{
-        console.log('Received : ', message);
+        engLog.push(message);
+        console.log('Received from ',message.name, ", engagement : ", message.eng);
     })
 }
 
+// setInterval(sendStatus, 5000);
+
 function sendStatus() {
 
-    console.log("send try");
+    if(engagementResult!=null){
+        console.log("send try");
 
-    socket.emit("message", engagementResult);
+        let toSend = new Object();
+        toSend.name = document.getElementById('studentName').value;
+        toSend.eng = engagementResult;
+        
+        socket.emit("message", toSend);
+    }
 
 }
